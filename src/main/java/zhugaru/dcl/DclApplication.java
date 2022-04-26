d72ef8ba9d56904fd318b9d2e61557ddf48b0211
Valerii <zzzhugaru@gmail.com>
2022-04-26 23:05:51 +0300

    Initial commit

---
+++ src/main/java/zhugaru/dcl/DclApplication.java
@@ -0,0 +1,13 @@
+package zhugaru.dcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DclApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zhugaru/dcl/DclApplicationTests.java
@@ -0,0 +1,13 @@
+package zhugaru.dcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
